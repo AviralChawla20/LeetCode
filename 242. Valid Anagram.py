7ff058d412d1ccb7c696f7d219e4fb179dc5f5c1
Aviral Chawla <aviralchawla02@gmail.com>
2024-01-13 10:01:04 +0530

    Create 242. Valid Anagram.py

---
+++ 242. Valid Anagram.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        first=[]
+        second=[]
+        for i in s:
+            first.append(i)
+        for i in t:
+            second.append(i)
+        first.sort()
+        second.sort()
+        if (first==second):
+            return True
+        else:
+            return False
+
+        
